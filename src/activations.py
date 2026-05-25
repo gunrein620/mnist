49718f83fa921806b1d57e905190389bed6979b4
kominsuk1064 <kms106418@gmail.com>
2026-05-25 14:44:53 +0900

    feat: ReLU, Softmax, Affine forward 구현

--- src/activations.py
+++ src/activations.py
@@ -27,6 +27,8 @@ class ReLU:
             x와 같은 shape. x > 0인 위치만 원래 값을 유지합니다.
         """
         # TODO: x > 0 위치를 self.mask에 저장하고, 음수/0 위치는 0으로 바꾸세요.
+        self.mask = x > 0
+        return x * self.mask
         raise NotImplementedError("ReLU.forward를 구현하세요.")
 
     def backward(self, dout):
@@ -59,6 +61,9 @@ class Softmax:
         """
         # TODO: 수치 안정성을 위해 row별 max를 뺀 뒤 softmax 확률을 계산하세요.
         # 힌트: np.max(..., axis=1, keepdims=True), np.exp, np.sum을 사용합니다.
+        x = x - np.max(x, axis=1, keepdims=True)
+        exp_x = np.ext(x)
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
         raise NotImplementedError("Softmax.forward를 구현하세요.")
 
     def backward(self, dout):
--- src/layers.py
+++ src/layers.py
@@ -33,6 +33,8 @@ class Affine:
             (batch_size, output_dim)
         """
         # TODO: backward에서 사용할 입력 x를 저장하고 x @ W + b를 반환하세요.
+        self.x = x
+        return x @ self.W + self.b
         raise NotImplementedError("Affine.forward를 구현하세요.")
 
     def backward(self, dout):
